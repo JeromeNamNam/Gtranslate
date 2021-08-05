522a59b9d3b85e5f12cc94ec27b4810a7618442d
JeromeNamNam <jnameche@gmail.com>
2021-08-05 23:31:21 +0200

    Update Gtranslate.py

--- Gtranslate.py
+++ Gtranslate.py
@@ -1,1 +1,6 @@
-python3 -m hello
+python3 -m Bonjour Jerome
+import googletrans
+from googletrans import Translator
+translator = Translator()
+result = translator.translate('Hello World, you are my man!', src='en',, dest='fr')
+print(result.text)
